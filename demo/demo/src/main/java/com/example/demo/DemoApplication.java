45fe661f57a8d82b11b711fa6563404692f911d3
Frankinator93 <francois.to@efrei.net>
2026-01-16 11:12:57 +0100

    Update DemoApplication.java

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +12,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+@RestController
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
